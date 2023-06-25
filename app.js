84c8b6bd8728b8ef846a2e72a7a2c5ae996211a7
Nicoluy Milovidov <milovidov.nicoluy@mail.ru>
2023-06-25 19:28:08 +0300

    исправил ошибку

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 
 const cols = document.querySelectorAll('.col')
 
-document.addEventListener('keydown', event => {
+document.addEventListener('keydown', (event) => {
     event.preventDefault()
     if (event.code.toLowerCase() === 'space') {
         setRandomColors()
     }
 })
 
-document.addEventListener('click', event => {
+document.addEventListener('click', (event) => {
     const type = event.target.dataset.type
     if (type === 'lock') {
-        const node = event.target.tagName.toLowerCase() === 'i'
+        const node =
+            event.target.tagName.toLowerCase() === 'i'
         ? event.target
         : event.target.children[0]
+
         node.classList.toggle('fa-lock-open')
         node.classList.toggle('fa-lock')
     }else if (type === 'copy') {
@@ -21,8 +23,7 @@ document.addEventListener('click', event => {
     }
 })
 
-   function gerenerateRandomColors() {
-
+   function gerenerateRandomColor() {
     const hexCodes = '0123456789ABCDEF'
     let color = ''
     for (let i = 0; i < 6; i++) {
@@ -31,15 +32,17 @@ document.addEventListener('click', event => {
     return '#' + color
 }
 
-function copyToClickboard(text) {
+   function copyToClickboard(text) {
     return navigator.clipboard.writeText(text)
 }
+
 
    function setRandomColors(isInitial) {
     const colors = isInitial ? getColorsFromHash() : []
 
     cols.forEach((col, index) => {
-        const isLocked = col.querySelector('i').classList .contains('fa-lock')
+
+        const isLocked = col.querySelector('i').classList.contains('fa-lock')
         const text = col.querySelector('h2')
         const button = col.querySelector('button')
 
@@ -68,17 +71,22 @@ function copyToClickboard(text) {
        updateColorsHash(colors)
 }
 
-function setTextColor(text, color) {
+   function setTextColor(text, color) {
 
     const luminance = chroma(color).luminance()
     text.style.color = luminance > 0.5 ? 'black' : 'white'
 
 }
 
-function updateColorsHash (colors = []) {
-    document.location.hash = colors.map((col) => {col.toString().substring(1)}).join('-')
+   function updateColorsHash (colors = []) {
+    document.location.hash = colors
+        .map((col) => {
+            return col.toString().substring(1)
+        })
+        .join('-')
 }
- function getColorsFromHash() {
+
+   function getColorsFromHash() {
     if (document.location.hash.length > 1) {
        return document.location.hash
            .substring(1)
